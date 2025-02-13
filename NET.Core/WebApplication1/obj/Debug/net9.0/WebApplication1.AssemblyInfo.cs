8640a739ac033aa34f67f49a638808c38f324bd4
Suyog Shinde <suyogs304@gmail.com>
2025-02-13 13:39:09 +0530

    modified:   NET.Core/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs 	modified:   NET.Core/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfoInputs.cache 	new file:   WebDev.sln

--- NET.Core/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ NET.Core/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3dd65c3e24df93783152842406db0b6401b864d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c10bb5e50762090dcb69d754a4cae4be522731")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
